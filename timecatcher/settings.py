89cb706bff03265378f69f12b06ed6506359bf41
GrahamJReid <grahamreidtechnology@gmail.com>
2023-10-08 18:19:16 -0500

    revertToCreateDeploy

--- timecatcher/settings.py
+++ timecatcher/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,8 +46,7 @@ INSTALLED_APPS = [
 
 CORS_ORIGIN_WHITELIST = (
     'http://localhost:3000',
-    'http://127.0.0.1:3000',
-    'https://time-catcher-server-580e22c73f6b.herokuapp.com'
+    'http://127.0.0.1:3000'
     
 )
 
